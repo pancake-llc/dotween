9413634b770850cb85a0904b6d8017d23c334c3b
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-01-27 10:36:52 +0700

    *Update postprocessor import package

--- Assets/_Root/Editor/ImportPackage.cs
+++ Assets/_Root/Editor/ImportPackage.cs
@@ -2,24 +2,36 @@
 using System.IO;
 using UnityEditor;
 
-
-public static class ImportPackage
+[InitializeOnLoad]
+public class ImportPackage : AssetPostprocessor
 {
-#if !LANCE_DOTWEEN_SUPPORT
     private const string DOTWEEN_PACKAGE_PATH = "Assets/_Root/Packages/dotween-lastest.unitypackage";
-    private const string PATH_INSTALL = "Assets/Plugins/Demigiant/DOTween";
+    private const string PATH_DOTWEEN_DLL = "Assets/Plugins/Demigiant/DOTween/DOTween.dll";
     private const string PACKAGE_PATH = "Packages/com.snorlax.dotween/Packages/dotween-lastest.unitypackage";
 
+    static ImportPackage() { EditorApplication.update += AutoImported; }
+
     [MenuItem("Package/Import DOTween")]
-    [InitializeOnLoadMethod]
     public static void ImportDoTween()
     {
-        if (Directory.Exists(PATH_INSTALL)) return;
         string path = DOTWEEN_PACKAGE_PATH;
         if (!File.Exists(path)) path = !File.Exists(Path.GetFullPath(PACKAGE_PATH)) ? DOTWEEN_PACKAGE_PATH : PACKAGE_PATH;
         AssetDatabase.ImportPackage(path, true);
     }
-#endif
+
+    public static void AutoImported()
+    {
+        EditorApplication.update -= AutoImported;
+        
+        if (!IsDotweenImported())
+        {
+            string path = DOTWEEN_PACKAGE_PATH;
+            if (!File.Exists(path)) path = !File.Exists(Path.GetFullPath(PACKAGE_PATH)) ? DOTWEEN_PACKAGE_PATH : PACKAGE_PATH;
+            AssetDatabase.ImportPackage(path, false);
+        }
+    }
+
+    public static bool IsDotweenImported() { return File.Exists(PATH_DOTWEEN_DLL); }
 }
 
 #endif
